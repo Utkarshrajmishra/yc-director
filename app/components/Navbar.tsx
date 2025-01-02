129cce4946e4a01db1e0e7fdfae55d9f82f2bf8b
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-01-02 20:40:11 +0530

    Fix: Auth Service

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,42 +1,52 @@
-import { auth } from "@/auth";
-import { signIn, signOut } from "next-auth/react";
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Navbar() {
-  const session = await auth();
-  const signOut = () => {};
+const Navbar= async()=>{
+  const session= await auth();
+
   return (
-    <>
-      <header className="px-5 py-3 bg-white shadow-m font-work-sans">
-        <nav className="flex justify-between items-center">
-          <Link href="/">
-            <Image src="/logo.png" alt="Logo Image" width={144} height={3} />
-          </Link>
-          <div className="flex items-center gap-5 text-black">
-            {session && session?.user ? (
-              <>
-                <Link href="/startup/create">
-                  <span>Create</span>
-                </Link>
-                <button onClick={signOut}></button>
-                <Link href={`/user/${session?.id}`}>
-                  <span>{session?.user?.name}</span>
-                </Link>
-              </>
-            ) : (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/">
+                <span>Create</span>
+              </Link>
               <form
                 action={async () => {
                   "use server";
-                  await signIn("github");
+                  await signOut();
                 }}
               >
-                <span>Login</span>
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
               </form>
-            )}
-          </div>
-        </nav>
-      </header>
-    </>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 }
+
+export default Navbar
